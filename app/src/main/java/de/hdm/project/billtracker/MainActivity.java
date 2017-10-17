ce8b6e3104da0ecbd461eafd98404c4b41fb88b5
Artur Bergen <ab176@hdm-stuttgart.de>
2017-10-17 12:49:05 +0200

    Add bottom actionbar

--- app/src/main/java/de/hdm/project/billtracker/MainActivity.java
+++ app/src/main/java/de/hdm/project/billtracker/MainActivity.java
@@ -1,13 +1,45 @@
 package de.hdm.project.billtracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_chart:
+                    mTextMessage.setText(R.string.title_chart);
+                    return true;
+                case R.id.navigation_camera:
+                    mTextMessage.setText(R.string.title_camera);
+                    return true;
+                case R.id.navigation_folder:
+                    mTextMessage.setText(R.string.title_folder);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
