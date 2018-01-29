0d029698eee79868b15d9cf4ef1b6db2723f1ee0
Valdet D <vd021@hdm-stuttgart.de>
2018-01-29 23:26:11 +0100

    added bill layout

---
+++ Billtracker/ViewControllers/BillCellTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  BillCellTableViewCell.swift
+//  Billtracker
+//
+//  Created by Valdet D. on 29.01.18.
+//  Copyright © 2018 Valdet D.. All rights reserved.
+//
+
+import UIKit
+
+class BillCellTableViewCell: UITableViewCell {
+
+
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
